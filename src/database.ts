641a002f6ba86c006abbd3c3f1dc09158e480e23
JoaoGRosa <joaooy@gmail.com>
2023-05-29 17:12:36 -0300

    aplicação_2

---
+++ src/database.ts
@@ -0,0 +1,82 @@
+import mysql from 'mysql'
+import { readJSONFile } from './leitordeJSON';
+
+export function criarConexao() {
+    
+    const jsonObject = readJSONFile();
+    var connection = mysql.createConnection({
+        host: jsonObject.host,
+        database: jsonObject.database,
+        user: jsonObject.user,
+        password: jsonObject.password
+    });
+
+    return connection;
+}
+
+
+interface alunos 
+{
+    "id": number,
+    "email": string,
+    "senha": string,
+    "nicho": string,
+    "nome": string
+}
+
+interface empresas 
+{
+    "id": number,
+    "email": string,
+    "senha": string,
+    "CNPJ": number,
+    "nome": string
+}
+
+interface professores 
+{
+    "id": number,
+    "email": string,
+    "senha": string,
+    "CPF": number,
+    "nome": string
+}
+
+interface Treinamentos 
+{
+    "id": number,
+    "nome_comercial": string,
+    "descricao": string,
+    "carga_horaria": number,
+    "comeco_fim_insc": string,
+    "comeco_fim_treinamento": string,
+    "qntd_min_insc": number,
+    "qntd_max_insc": number,
+  }
+  
+
+
+export function criaAluno(objeto : alunos){
+    var comando = "INSERT INTO alunos (id, nome,senha,email,nicho) values (0,\"" +
+    objeto.nome + "\", \"" + objeto.senha + "\", \"" + objeto.email + "\", \"" + objeto.nicho + "\");";
+    return comando;
+}
+
+export function criaEmpresas(objeto : empresas){
+    var comando = "INSERT INTO empresas (id,nome,senha,email,CNPJ) values (0,\"" +
+    objeto.nome + "\", \"" + objeto.senha + "\", \"" + objeto.email + "\", \"" + objeto.CNPJ + "\");";
+    return comando;
+}
+
+export function criaProfessores(objeto : professores){
+    var comando = "INSERT INTO professores (id,nome,senha,email,CPF) values (0,\"" +
+    objeto.nome + "\", \"" + objeto.senha + "\", \"" + objeto.email + "\", \"" + objeto.CPF + "\");";
+    return comando;
+}
+
+export function criaTreinamentos (objeto: Treinamentos){
+    var comando = "INSERT INTO treinamentos (id, nome_comercial, descricao, carga_horaria, comeco_fim_insc, comeco_fim_treinamento, qntd_min_insc, qntd_max_insc) values (0,\""+
+    objeto.nome_comercial +"\",\"" + objeto.descricao + "\",\"" + objeto.carga_horaria + "\",\"" + objeto.comeco_fim_insc + "\",\"" + objeto.comeco_fim_treinamento + "\",\"" + objeto.qntd_max_insc + "\",\"" + objeto.qntd_min_insc + "\");";
+    return comando;
+  } 
+  
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,73 @@
 import express from 'express'
 import cors from 'cors'
+import bodyParser from 'body-parser';
+import { criaAluno, criarConexao, criaEmpresas, criaProfessores, criaTreinamentos } from './database';
+import { readJSON } from './leitordeJSON';
+
+
+var connection = criarConexao();
+
 // Porta do servidor
-const PORT = process.env.PORT || 4000
+const PORT = 8000
 // Host do servidor
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost'
 // App Express
 const app = express()
+// Faz com que o body da requisição http vire um json
+app.use(bodyParser.json());
 // Endpoint raiz
 app.get('/', (req, res) => {
-    res.send('Bem-vindo!')
+    let sql = "SELECT * FROM alunos";
+    connection.query(sql, function(err, results){
+        if (err) throw err;
+        res.send(results);
+    });
 })
+
+app.post('/login', (req, res) => {
+    var body = req.body;
+    console.log(body);
+    let comando = "SELECT * FROM " + body.usuario + " where email = \"" + body.email + "\" and senha = \"" + body.senha + "\"";
+    console.log(comando);
+    connection.query(comando, function(err, results){
+        if (err) throw err;
+        console.log(results[0].id); //precisa de [0], pq o "results" devolve uma array com todos os "rows" encontrados em forma de objeto, 
+        //como sempre sera apenas 1 usuario ou nenhum, usamos o index 0, para acessar o objeto enviado.
+        res.send(JSON.stringify(results[0])); // passamos o objeto para JSON e devolvemos.
+    });    
+})
+
+//Sucesso
+app.post('/cadastro', (req, res) => {
+    var body = req.body;
+    var comando : any;
+    if(body.usuario == "alunos")
+        comando = criaAluno(body);
+    else if(body.usuario == "empresas")
+        comando = criaEmpresas(body);
+    else if(body.usuario == "professores")
+        comando = criaProfessores(body);
+    console.log(comando);
+    connection.query(comando, function(err, results){
+        if (err) throw err;
+        res.send(results);
+    });   
+})
+app.post('/treinamentos', (req,res)=>{
+var body = req.body;
+var comando : any;
+comando = criaTreinamentos(body);
+console.log(comando);
+connection.query(comando,function(err, results){
+    if (err)throw err;
+    res.send(results);
+})
+
+
+});
 // Cors
 app.use(cors({
-    origin: ['http://localhost:3000']
+    origin: ['http://localhost:8000']
 }))
 // Resposta padrão para quaisquer outras requisições:
 app.use((req, res) => {
